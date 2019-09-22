7582f4aeb32e383d0e401ea94827ae79bd907d4a
anbubsc2k14 <anbubsc2k14@gmail.com>
2019-09-22 14:05:48 +0530

    Add files via upload

--- ops.py
+++ ops.py
@@ -13,11 +13,11 @@ try:
     merge_summary = tf.merge_summary
     SummaryWriter = tf.train.SummaryWriter
 except:
-    image_summary = tf.summary.image
-    scalar_summary = tf.summary.scalar
-    histogram_summary = tf.summary.histogram
-    merge_summary = tf.summary.merge
-    SummaryWriter = tf.summary.FileWriter
+    image_summary = tf.compat.v1.summary.image
+    scalar_summary = tf.compat.v1.summary.scalar
+    histogram_summary = tf.compat.v1.summary.histogram
+    merge_summary = tf.compat.v1.summary.merge
+    SummaryWriter = tf.compat.v1.summary.FileWriter
 
 if "concat_v2" in dir(tf):
     def concat(tensors, axis, *args, **kwargs):
@@ -29,7 +29,7 @@ else:
 
 class batch_norm(object):
     def __init__(self, epsilon=1e-5, momentum=0.9, name="batch_norm"):
-        with tf.variable_scope(name):
+        with tf.compat.v1.variable_scope(name):
             self.epsilon = epsilon
             self.momentum = momentum
             self.name = name
@@ -55,7 +55,7 @@ def conv_cond_concat(x, y):
 def conv2d(input_, output_dim,
            k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
            name="conv2d"):
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
                             initializer=tf.truncated_normal_initializer(stddev=stddev))
 
@@ -70,7 +70,7 @@ def conv2d(input_, output_dim,
 def deconv2d(input_, output_shape,
              k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
              name="deconv2d", with_w=False):
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
 
         # filter : [height, width, output_channels, in_channels]
         w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
@@ -103,7 +103,7 @@ def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=
 
     # print( "Linear shape = " + str(shape) )
 
-    with tf.variable_scope(scope or "Linear"):
+    with tf.compat.v1.variable_scope(scope or "Linear"):
 
         matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
                                  tf.random_normal_initializer(stddev=stddev))
